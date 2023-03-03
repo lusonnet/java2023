a85f7c4d4156fcebe2cf8c196d42cdfe58e2bd4f
lusonnet <lulu_2001@foxmail.com>
2023-03-03 23:54:44 +0800

    刷题

---
+++ 3_3/src/Demo1.java
@@ -0,0 +1,35 @@
+/**
+ * @ClassName: Demo1
+ * @Description:牛客网top第一道
+ * @author: sonnet
+ * @date: 2023/3/3 23:48
+ */
+
+
+public class Demo1 {
+}
+
+//public class ListNode {
+//    int val;
+//    ListNode next = null;
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//}
+//public class Solution {
+//    public ListNode ReverseList(ListNode head) {
+//        if(head == null)return null;
+//        ListNode pre = null;
+//        ListNode cur = null;
+//        while(head != null){
+//            cur = head.next;
+//            head.next = pre;
+//            pre = head;
+//            head = cur;
+//
+//        }
+//        return pre;
+//
+//    }
+//}
